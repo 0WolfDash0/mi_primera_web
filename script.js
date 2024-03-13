a4e87751b847d9998dba21b0207d7204824186d6
0WolfDash0 <163215395+0WolfDash0@users.noreply.github.com>
2024-03-12 19:31:28 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+//alert("Hola este es mi Javascript");
+
+//let nombre = "KIM";
+//nombre = "Lee"; 
+
+//var apellido = "Valenzuela";
+
+//const apellido2 = "Chung";
+
+
+//console.log(nombre);
+//console.log(apellido);
+//console.log(apellido2);
+
+
+let contenidoTitulo = "KIM";
+var titulo = document.querySelector(".acerca3");
+titulo.innerHTML = contenidoTitulo;
+
+//CONDICIONALES
+
+let textoTitulo = titulo.innerHTML;
+console.log(textoTitulo);
+
+//if ()
